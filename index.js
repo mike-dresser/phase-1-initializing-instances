5fdb8139cca53f5d0e8f4d8f94fe4e6e2ed6e78d
Mike Dresser <116307817+michaeldresser81@users.noreply.github.com>
2024-02-16 15:19:51 -0500

    complete

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-// Write your code here
+class Breakfast {
+  constructor(food, drink) {
+    this.food = food;
+    this.drink = drink;
+  }
+}
+
+class Lunch {
+  constructor(salad, soup, drink) {
+    this.salad = salad;
+    this.soup = soup;
+    this.drink = drink;
+  }
+}
+
+class Dinner {
+  #dessert;
+  constructor(salad, soup, entree, dessert) {
+    this.#dessert = dessert;
+    this.soup = soup;
+    this.salad = salad;
+    this.entree = entree;
+  }
+  sweets() {
+    console.log(this.#dessert);
+  }
+}
+
+let x = new Dinner('pot de creme');
+// console.log(`dessert: ${x.#dessert}`);
+
+class C {
+  #x;
+  constructor(x) {
+    this.#x = x;
+  }
+  static getX(obj) {
+    if (#x in obj) return obj.#x;
+
+    return 'obj must be an instance of C';
+  }
+}
+class ClassWithPrivateField {
+  #privateField = 50;
+
+  constructor() {}
+}
